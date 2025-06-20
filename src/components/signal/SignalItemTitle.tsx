e1bc482ba6a9df1ae9e231b0ab82637041592cb9
K-KY <kyuyoungk@naver.com>
2025-06-20 11:53:02 +0900

    Refactor : 공통 CSS 분리 및 각 속성 이름 추가

--- src/components/signal/SignalItemTitle.tsx
+++ src/components/signal/SignalItemTitle.tsx
@@ -3,12 +3,12 @@ import "../../css/signal/SignalItem.css"
 const SignalItemTitle = ({title}: { title: string }) => {
     if (title.toLowerCase() === "long") {
         return (
-            <div className={"title-position-long"}>{title}</div>
+            <div className={"title-position-long item-title"}>{title}</div>
         )
     }
     if (title.toLowerCase() === "short") {
         return (
-            <div className={"title-position-short"}>{title}</div>
+            <div className={"title-position-short item-title"}>{title}</div>
         )
     }
 }
