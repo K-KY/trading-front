88f6ee2a3b0935f0426cbe6fe19e7d4fd2801f36
K-KY <kyuyoungk@naver.com>
2025-06-29 23:46:29 +0900

    Feat : api 호출 예외처리

--- src/components/api/axios/MonitorService.ts
+++ src/components/api/axios/MonitorService.ts
@@ -3,9 +3,13 @@ import {baseURL} from "./BaseURL.ts";
 
 
 const getWorkers = async() => {
-    return await axios.get(baseURL+'/workers')
-        .then(response => response.data)
-        .catch(error => console.log(error));
+    try {
+        const response = await axios.get(baseURL + '/workers');
+        return response.data;
+    } catch (error) {
+        console.error("getWorkers 에러:", error);
+        throw error; // 상위에서 처리하도록 예외 던짐
+    }
 }
 
 
